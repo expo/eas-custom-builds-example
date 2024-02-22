b8fe52b4d87ab27e01a8dd8026152ef0e0a25068
Stanisław Chmiela <sjchmiela@gmail.com>
2024-02-22 21:33:34 +0100

    Update development build profile

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [isSectionExpanded, setIsSectionExpanded] = useState(false);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
+      <Button title="Expand" onPress={() => setIsSectionExpanded(true)} />
+      {isSectionExpanded && <Text>Additional information</Text>}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
