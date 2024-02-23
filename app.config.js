2ba26965bd5fb14536d63ac569ad96affac65d44
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2024-02-23 15:55:12 +0100

    Add build-and-maestro-test build profile (#17)

--- app.config.js
+++ app.config.js
@@ -1,52 +1,56 @@
-import assert from 'assert';
+import assert from "assert";
 
 module.exports = {
   expo: {
-    name: 'eas-custom-builds-example',
-    slug: 'eas-custom-builds-example',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './assets/icon.png',
-    userInterfaceStyle: 'light',
+    name: "eas-custom-builds-example",
+    slug: "eas-custom-builds-example",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
     splash: {
-      image: './assets/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff',
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
     updates: {
       fallbackToCacheTimeout: 0,
     },
-    assetBundlePatterns: ['**/*'],
+    assetBundlePatterns: ["**/*"],
     ios: {
       supportsTablet: true,
-      bundleIdentifier: 'com.exponent.eascustombuildsexample',
+      bundleIdentifier: "dev.expo.eascustombuildsexample",
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
-        backgroundColor: '#FFFFFF',
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF",
       },
-      package: 'com.exponent.eascustombuildsexample',
+      package: "dev.expo.eascustombuildsexample",
     },
     web: {
-      favicon: './assets/favicon.png',
+      favicon: "./assets/favicon.png",
     },
     extra: {
       eas: {
         projectId: resolveProjectId(),
       },
     },
-    owner: 'exponent',
+    // In local environment we're usually signed in as the owner.
+    owner: process.env.EXPO_LOCAL ? undefined : "exponent",
   },
 };
 
 function resolveProjectId() {
   if (process.env.EXPO_LOCAL) {
-    assert(process.env.LOCAL_PROJECT_ID, 'Set LOCAL_PROJECT_ID in .envrc.local.');
+    assert(
+      process.env.LOCAL_PROJECT_ID,
+      "Set LOCAL_PROJECT_ID in .envrc.local.",
+    );
     return process.env.LOCAL_PROJECT_ID;
   } else if (process.env.EXPO_STAGING) {
-    return '59284232-d5fe-40ee-8f4e-4e9bf74aad78';
+    return "59284232-d5fe-40ee-8f4e-4e9bf74aad78";
   } else {
-    return 'b06749c9-fe1e-481a-a655-70b6d26aa5d2';
+    return "b06749c9-fe1e-481a-a655-70b6d26aa5d2";
   }
 }
