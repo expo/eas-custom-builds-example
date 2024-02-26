b2a82443cfcdf18d8819117ff2729bae93c97179
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2024-02-26 09:34:10 +0100

    Add examples with expanded `eas/build` step (#21)

--- app.config.js
+++ app.config.js
@@ -1,35 +1,39 @@
-import assert from "assert";
+import assert from 'assert';
 
 module.exports = {
   expo: {
-    name: "eas-custom-builds-example",
-    slug: "eas-custom-builds-example",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
+    name: 'eas-custom-builds-example',
+    slug: 'eas-custom-builds-example',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
     splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff",
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
     },
     updates: {
       fallbackToCacheTimeout: 0,
+      url: 'https://u.expo.dev/b06749c9-fe1e-481a-a655-70b6d26aa5d2',
     },
-    assetBundlePatterns: ["**/*"],
+    runtimeVersion: {
+      policy: 'appVersion',
+    },
+    assetBundlePatterns: ['**/*'],
     ios: {
       supportsTablet: true,
-      bundleIdentifier: "dev.expo.eascustombuildsexample",
+      bundleIdentifier: 'dev.expo.eascustombuildsexample',
     },
     android: {
       adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#FFFFFF",
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
       },
-      package: "dev.expo.eascustombuildsexample",
+      package: 'dev.expo.eascustombuildsexample',
     },
     web: {
-      favicon: "./assets/favicon.png",
+      favicon: './assets/favicon.png',
     },
     extra: {
       eas: {
@@ -37,7 +41,7 @@ module.exports = {
       },
     },
     // In local environment we're usually signed in as the owner.
-    owner: process.env.EXPO_LOCAL ? undefined : "exponent",
+    owner: process.env.EXPO_LOCAL ? undefined : 'exponent',
   },
 };
 
@@ -45,12 +49,12 @@ function resolveProjectId() {
   if (process.env.EXPO_LOCAL) {
     assert(
       process.env.LOCAL_PROJECT_ID,
-      "Set LOCAL_PROJECT_ID in .envrc.local.",
+      'Set LOCAL_PROJECT_ID in .envrc.local.'
     );
     return process.env.LOCAL_PROJECT_ID;
   } else if (process.env.EXPO_STAGING) {
-    return "59284232-d5fe-40ee-8f4e-4e9bf74aad78";
+    return '59284232-d5fe-40ee-8f4e-4e9bf74aad78';
   } else {
-    return "b06749c9-fe1e-481a-a655-70b6d26aa5d2";
+    return 'b06749c9-fe1e-481a-a655-70b6d26aa5d2';
   }
 }
